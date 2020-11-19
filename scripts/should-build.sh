0d5c3cd92f328bbded0fd5c53c211931a95ef9b6
Alex Carney <alcarneyme@gmail.com>
2020-11-19 15:32:03 +0000

    Start work on an integrated workflow

---
+++ scripts/should-build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Script to check if we should build a given component or not.
+
+# File patterns to check for each component, if there's a match a build will be
+# triggered
+VSCODE="^code"
+PYTHON="^lib/esbonio"
+
+# Determine which files have changed
+files=$(git diff --name-only ${BASE}..HEAD)
+echo -e "Files Changed:\n\n$files"
+
+case $1 in
+    vscode)
+        PATTERN=${VSCODE}
+        ;;
+    python)
+        PATTERN=${PYTHON}
+        ;;
+    *)
+        echo "Unknown component ${1}"
+        exit 1
+        ;;
+esac
+
+changes=$(echo $files | grep -E ${PATTERN})
+echo
+
+if [ -z "$changes" ]; then
+    echo "There is nothing to do."
+else
+    echo "Changes detected, doing build!"
+    echo "::set-output name=build::true"
+fi
