4762bdcf6bea326c00f21505e89c54b25d774c0c
Alex Carney <alcarneyme@gmail.com>
2020-11-20 19:26:05 +0000

    Automatically create Github releases when doing a PyPi release (#4)

--- lib/esbonio/README.md
+++ lib/esbonio/README.md
@@ -4,5 +4,5 @@ Esbonio - to explain.
 
 **This package is in early development**
 
-A Python package that provides a number of Sphinx extensions and a [Language Server]() for enhancing
+A Python package that provides a number of Sphinx extensions and a [Language Server](https://microsoft.github.io/language-server-protocol/) for enhancing
 the editing experience of `*.rst` files in editors.
---
+++ scripts/make-dev-version.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Make a dev release number based on the PR's issue number.
+
+set -e
+
+SRC=
+
+case $1 in
+    vscode)
+        ;;
+    python)
+        SRC="lib/esbonio"
+        ;;
+    *)
+        echo "Unkown component ${1}"
+        exit 1
+        ;;
+esac
+
+if [ -z "${SRC}" ]; then
+    echo "SRC dir is not set!"
+    exit 1
+fi
+
+cd ${SRC}
+
+version=$(grep 'current_version' .bumpversion.cfg | sed 's/.*=\s\(.*\)/\1/')
+build=$(echo $GITHUB_REF | sed -E 's/.*\/([0-9]+)\/.*/\1/')
+
+echo "ref: ${GITHUB_REF}"
+echo "Current Version: ${version}"
+echo "Build number is ${build}"
+echo
+
+VERSION="${version}.dev${build}"
+echo "Dev version number is: ${VERSION}"
+
+python -m bumpversion --new-version "${VERSION}" dev
