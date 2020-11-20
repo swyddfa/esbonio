12d0c909e901931c2f54daf6815611258e1554d5
Alex Carney <alcarneyme@gmail.com>
2020-11-20 23:11:08 +0000

    Use towncrier to generate release notes as part of a workflow (#5)
    - Have towncrier generate the release notes and changelog entries from a `changes/` folder
    - Instead of using commit messages as the basis of determining the next version number, use the changelog!
      + At least one entry of the form `<N>.breaking.rst` will trigger a `major` version bump
      + At least one entry of the form `<N>.feature.rst` will trigger a `minor` version bump
      + Otherwise we'll use a `patch` version bump

--- scripts/make-release.sh
+++ scripts/make-release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Book keeping around making a release. This script will
 #
-# - Call bumpversion to generate a new version based on the commit message
+# - Call bumpversion to generate a new version based on the changelog
 # - Commit, tag and push the new version number.
 # - Export the tag name and release date for use later on in the pipeline.
 set -e
@@ -41,34 +41,73 @@ fi
 
 cd ${SRC}
 
-message=$(git log HEAD --pretty=format:'%s' | head -n 1 | tr '[:upper:]' '[:lower:]')
-echo "Commit message: ${message}"
+# Make sure there's at least some changes...
+if [ -z "$(find changes -name '*.rst')" ]; then
+    echo "There are no changes!"
+    exit 1
+fi
 
-case $message in
-    major*)
-        KIND="major";;
-    minor*)
-        KIND="minor";;
-    *)
-        KIND="patch";;
-esac
+# Use the changelog to determine the type of release to make.
+if [ ! -z "$(find changes -name '*.breaking.rst')" ]; then
+    echo "Breaking changes found, doing major release!"
+    KIND="major"
+elif [ ! -z "$(find changes -name '*.feature.rst')" ]; then
+    echo "New features found, doing minor release!"
+    KIND="minor"
+else
+    echo "Nothing significant detected, doing patch release"
+    KIND="patch"
+fi
 
+# Bump the version accordingly
 python -m bumpversion ${KIND}
 VERSION=$(grep 'current_version' .bumpversion.cfg | sed 's/.*=\s\(.*\)/\1/')
 
-git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
-git config user.name "github-actions[bot]"
+# If we're in a PR build, make a dev version number based on it.
+if [[ "${GITHUB_REF}" == refs/pull/* ]]; then
+
+    BUILD=$(echo $GITHUB_REF | sed -E 's/.*\/([0-9]+)\/.*/\1/')
+
+    echo
+    echo "ref: ${GITHUB_REF}"
+    echo "Current Version: ${VERSION}"
+    echo "Build number is ${BUILD}"
+    echo
+
+    VERSION="${VERSION}.dev${BUILD}"
+    echo "Dev version number is: ${VERSION}"
+
+    python -m bumpversion --allow-dirty --new-version "${VERSION}" dev
+
+fi
 
 TAG="${TAG_PREFIX}${VERSION}"
 DATE=$(date +%Y-%m-%d)
 
-git commit -am "${COMMIT_MSG}${VERSION}"
-git tag -a "${TAG}" -m "${COMMIT_MSG}${VERSION}"
 
-git push origin release
-git push origin --tags
+# Only if we are on the release branch.
+if [ "${GITHUB_REF}" = "refs/head/release" ]; then
 
-# Export info that can be picked up in later steps.
-echo "::set-output name=VERSION::${VERSION}"
-echo "::set-output name=TAG::${TAG}"
-echo "::set-output name=DATE::${DATE}"
+    # Write the release notes for github
+    python -m towncrier --draft --version="${VERSION}" | \
+        rst2html.py --template=changes/github-template.html > .changes.html
+
+    # Write the release notes for the changelog
+    python -m towncrier --version="${VERSION}"
+
+    # Setup git, commit, tag and push all the changes.
+    git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
+    git config user.name "github-actions[bot]"
+
+    git commit -am "${COMMIT_MSG}${VERSION}"
+    git tag -a "${TAG}" -m "${COMMIT_MSG}${VERSION}"
+
+    git push origin release
+    git push origin --tags
+
+    # Export info that can be picked up in later steps.
+    echo "::set-output name=VERSION::${VERSION}"
+    echo "::set-output name=TAG::${TAG}"
+    echo "::set-output name=DATE::${DATE}"
+
+fi
