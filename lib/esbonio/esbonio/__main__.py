5626723efad75680749e07cb13e5e27110e78edd
Alex Carney <alcarneyme@gmail.com>
2020-12-04 19:12:27 +0000

    Publish python logging events to the Language Client (#27)

--- lib/esbonio/esbonio/__main__.py
+++ lib/esbonio/esbonio/__main__.py
@@ -1,36 +1,37 @@
 import argparse
 import logging
-import pathlib
 import sys
 
-import appdirs
 import esbonio.lsp as lsp
 
 from esbonio import __version__
+from esbonio.lsp.logger import LspHandler
 
 LOG_LEVELS = [logging.ERROR, logging.INFO, logging.DEBUG]
+
+
+def configure_logging(verbose, server):
+
+    try:
+        level = LOG_LEVELS[-1]
+    except IndexError:
+        level = LOG_LEVELS[-1]
+
+    logger = logging.getLogger("esbonio")
+    logger.setLevel(level)
+
+    handler = LspHandler(server)
+    handler.setLevel(level)
+
+    formatter = logging.Formatter("[%(name)s] %(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
 
 
 def start_server(verbose):
     """Start the language server."""
 
-    try:
-        level = LOG_LEVELS[verbose]
-    except IndexError:
-        level = LOG_LEVELS[-1]
-
-    logdir = pathlib.Path(appdirs.user_log_dir("esbonio", "swyddfa"))
-    if not logdir.exists():
-        logdir.mkdir(parents=True)
-
-    logfile = logdir / "language_server.log"
-    logging.basicConfig(
-        level=level,
-        format="[%(levelname)s][%(name)s]: %(message)s",
-        filemode="w",
-        filename=str(logfile),
-    )
-
+    configure_logging(verbose, lsp.server)
     lsp.server.start_io()
 
 
