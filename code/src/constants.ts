efde066d29dcf5f38a695db166ec32f849e0d9fb
Alex Carney <alcarneyme@gmail.com>
2021-05-12 19:06:57 +0100

    vscode: Refactor language server bootstrap code
    This refactors the lanuage server bootstrap code so that it's more
    centralised and the concerns are better separated. Apart from the
    main `extension.ts` file, all language server code resides in the
    `src/lsp` folder.
    
    This folder now contains three files
    - `python.ts` contains the `PythonManager` class which is responsible
      for finding a suitable Python environment and running commands within
      it.
    - `server.ts` contains the `ServerManager` class which is responsible
      for installing and updating the `esbonio` python package which
      contains the language server.
    - Finally, `client.ts` contains the `ClientManager` class which is
      responsible for running the server with the correct arguments and
      creating the appropriate `LanguageClient` instance to tie it into
      vscode.

---
+++ code/src/constants.ts
@@ -0,0 +1,13 @@
+export const PYTHON_EXTENSION = "ms-python.python"
+
+
+export namespace Server {
+  export const REQUIRED_PYTHON = "3.6.0"
+  export const REQUIRED_VERSION = "0.5.0"
+}
+
+export namespace Commands {
+  export const INSTALL_SERVER = "esbonio.server.install"
+  export const RESTART_SERVER = "esbonio.server.restart"
+  export const UPDATE_SERVER = "esbonio.server.update"
+}
