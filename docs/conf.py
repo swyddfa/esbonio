f9d9c563c550fc21ce3dd37c1e9a019d3ebc6b44
Alex Carney <alcarneyme@gmail.com>
2021-05-14 00:52:20 +0100

    docs: Switch to sphinx-book-theme

--- docs/conf.py
+++ docs/conf.py
@@ -64,13 +64,15 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
-html_context = {
-    "conf_py_path": "/docs/",
-    "display_github": True,
-    "github_repo": "esbonio",
-    "github_user": "swyddfa",
-    "github_version": "release",
+html_theme = "sphinx_book_theme"
+
+html_theme_options = {
+    "repository_url": "https://github.com/swyddfa/esbonio",
+    "use_repository_button": True,
+    "use_issues_button": True,
+    "use_edit_page_button": True,
+    "repository_branch": "release",
+    "path_to_docs": "docs/",
 }
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
