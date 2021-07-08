294a52c062426cd6f259d5bb651235222b1ab2ef
Alex Carney <alcarneyme@gmail.com>
2021-07-08 15:17:34 +0100

    lsp: Reintroduce the `RstLanguageServer` class
    The main idea behind the next series of commits is to have the
    language server be the source of truth for all the information that
    `LanguageFeature`s will query when processing whatever they need to
    process.
    
    The `RstLanguageServer` class still serves as the base that everything
    will build on but it is now additionally responsible for providing
    information on anything that is handled directly by `docutils`.
    Currently this translates into indexing any role or directive
    definitions that are registered directly with docutils.
    
    Additionally this class is now responsible for managing all
    diagnostics that are to be pushed to lanaguge clients.
    `LanguageFeature`s that wish to provide diagnostics must now specify a
    `source` as well as the `textDocument`'s URI.

---
+++ lib/esbonio/esbonio/lsp/feature.py
@@ -0,0 +1,53 @@
+import typing
+from typing import List
+
+from pygls.lsp.types import CompletionItem
+from pygls.lsp.types import DidSaveTextDocumentParams
+from pygls.lsp.types import InitializedParams
+from pygls.lsp.types import InitializeParams
+from pygls.lsp.types import Position
+from pygls.workspace import Document
+
+if typing.TYPE_CHECKING:
+    import re
+    from .rst import RstLanguageServer
+
+
+class LanguageFeature:
+    """Base class for language features."""
+
+    def __init__(self, rst: "RstLanguageServer"):
+        self.rst = rst
+        self.logger = rst.logger.getChild(self.__class__.__name__)
+
+    def initialize(self, options: InitializeParams) -> None:
+        """Called once when the server is first initialized."""
+
+    def initialized(self, params: InitializedParams) -> None:
+        """Called once upon receipt of the `initialized` notification from the client."""
+
+    def save(self, params: DidSaveTextDocumentParams) -> None:
+        """Called each time a document is saved."""
+
+    completion_triggers = []
+    """A list of regular expressions used to determine if the :meth`~LanguageFeature.complete`
+    method should be called on the current line."""
+
+    def complete(
+        self, match: "re.Match", doc: Document, pos: Position
+    ) -> List[CompletionItem]:
+        """Called if any of the given ``completion_triggers`` match the current line.
+
+        This method should return a list of ``CompletionItem`` objects to return to the
+        client.
+
+        Parameters
+        ----------
+        match:
+           The match object generated from the corresponding regular expression.
+        doc:
+           The document the completion has been requested within
+        pos:
+           The position of the request within the document.
+        """
+        return []
