8b6a2a1d8e690b0a2eba6c6cef377f253ffed564
Alex Carney <alcarneyme@gmail.com>
2021-07-09 15:46:37 +0100

    lsp: Add domain completion provider
    The original completion code from the `Roles` language feature has
    been pulled out into its own class and has been merged with the
    completion code from the old `InterSphinx` language feature.

---
+++ lib/esbonio/esbonio/lsp/completion/domains.py
@@ -0,0 +1,107 @@
+"""Completion provider for Sphinx domains."""
+import re
+from typing import List
+from typing import Optional
+
+from pygls.lsp.types import CompletionItem
+from pygls.lsp.types import CompletionItemKind
+from pygls.workspace import Document
+
+from esbonio.lsp.roles import TargetCompletion
+from esbonio.lsp.sphinx import SphinxLanguageServer
+
+
+TARGET_KINDS = {
+    "attribute": CompletionItemKind.Field,
+    "doc": CompletionItemKind.File,
+    "class": CompletionItemKind.Class,
+    "envvar": CompletionItemKind.Variable,
+    "function": CompletionItemKind.Function,
+    "method": CompletionItemKind.Method,
+    "module": CompletionItemKind.Module,
+    "term": CompletionItemKind.Text,
+}
+
+
+def intersphinx_target_to_completion_item(
+    label: str, target: tuple, type_: str
+) -> CompletionItem:
+
+    # _. _. url, _
+    source, version, _, display = target
+
+    display_name = label if display == "-" else display
+    completion_kind = ":".join(type_.split(":")[1:]) if ":" in type_ else type_
+
+    if version:
+        version = f" v{version}"
+
+    return CompletionItem(
+        label=label,
+        detail=f"{display_name} - {source}{version}",
+        kind=TARGET_KINDS.get(completion_kind, CompletionItemKind.Reference),
+        insert_text=label,
+    )
+
+
+def object_to_completion_item(object_: tuple) -> CompletionItem:
+
+    # _, _, _, docname, anchor, priority
+    name, display_name, type_, _, _, _ = object_
+
+    key = type_.split(":")[1] if ":" in type_ else type_
+    kind = TARGET_KINDS.get(key, CompletionItemKind.Reference)
+
+    insert_text = f"/{name}" if type_ == "doc" else name
+
+    return CompletionItem(
+        label=name, kind=kind, detail=str(display_name), insert_text=insert_text
+    )
+
+
+def project_to_completion_item(project: str) -> CompletionItem:
+    return CompletionItem(
+        label=project, detail="intersphinx", kind=CompletionItemKind.Module
+    )
+
+
+class Domain(TargetCompletion):
+    def __init__(self, rst: SphinxLanguageServer):
+        self.rst = rst
+        self.logger = rst.logger.getChild(self.__class__.__name__)
+
+    def complete_targets(
+        self, doc: Document, match: "re.Match", name: str, domain: Optional[str]
+    ) -> List[CompletionItem]:
+
+        groups = match.groupdict()
+        target = groups["target"]
+
+        if ":" in target:
+            return self.complete_intersphinx_targets(name, domain, target)
+
+        items = [
+            object_to_completion_item(o)
+            for o in self.rst.get_role_targets(name, domain)
+        ]
+
+        items += [
+            project_to_completion_item(p) for p in self.rst.get_intersphinx_projects()
+        ]
+
+        return items
+
+    def complete_intersphinx_targets(
+        self, name, domain, target
+    ) -> List[CompletionItem]:
+        items = []
+        project, *_ = target.split(":")
+        intersphinx_targets = self.rst.get_intersphinx_targets(project, name, domain)
+
+        for type_, targets in intersphinx_targets.items():
+            items += [
+                intersphinx_target_to_completion_item(label, target, type_)
+                for label, target in targets.items()
+            ]
+
+        return items
