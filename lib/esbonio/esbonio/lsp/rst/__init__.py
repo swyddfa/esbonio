90bde7fedb4820ca98575fe8fb128f17169ce13c
Alex Carney <alcarneyme@gmail.com>
2022-07-31 18:41:52 +0100

    lsp: Use a text document's language id where possible
    When identifying where a given position is (python vs rst vs docstring)
    use the document's `lanuage_id` field where possible. This handles
    situations where a user has configured `*.txt` files in VSCode to be
    treated as rst files.
    
    If the document's language id is not available, then we fall back to
    using file extensions as we did previously

--- lib/esbonio/esbonio/lsp/rst/__init__.py
+++ lib/esbonio/esbonio/lsp/rst/__init__.py
@@ -688,32 +688,39 @@ class RstLanguageServer(LanguageServer):
             self.logger.debug("Publishing %d diagnostics for: %s", len(diag_list), uri)
             self.publish_diagnostics(uri, diag_list.data)
 
-    def get_location_type(self, doc: Document, position: Position) -> str:
-        """Given a document and a position, return the kind of location that
-        represents.
+    def get_location_type(self, document: Document, position: Position) -> str:
+        """Given a document and a position, return the type of location.
 
-        This will return one of the following values:
+        Returns one of the following values:
 
-        - ``rst``: Indicates that the position is within an ``*.rst`` document
-        - ``py``: Indicates that the position is within code in a ``*.py`` document
+        - ``rst``: Indicates that the position is within an reStructuredText document
+        - ``py``: Indicates that the position is within code in a Python file
         - ``docstring``: Indicates that the position is within a docstring in a
-          ``*.py`` document.
+          Python file.
+
+        If the location type cannot be determined, this function will fall back to
+        ``rst``.
 
         Parameters
         ----------
-        doc:
+        doc
            The document associated with the given position
-        position:
-           The position to determine the type of.
-        """
-        ext = pathlib.Path(Uri.to_fs_path(doc.uri)).suffix
 
-        if ext == ".rst":
+        position
+           The position to determine the type of
+        """
+        doc = self.workspace.get_document(document.uri)
+        fpath = pathlib.Path(Uri.to_fs_path(doc.uri))
+
+        # Prefer the document's language_id, but fallback to file extensions
+        loctype = getattr(doc, "language_id", None) or fpath.suffix
+
+        if loctype in {".rst", "rst", "restructuredtext"}:
             return "rst"
 
-        if ext == ".py":
+        if loctype in {".py", "py", "python"}:
 
-            # Let's count how many pairs of triple quotes are "above" us in the file
+            # Let's count how many pairs of triple quotes are above us in the file
             # even => we're outside a docstring
             # odd  => we're within a docstring
             source = self.text_to_position(doc, position)
