f45f39463054937a4dc59c54be0c45a40c295cbc
Alex Carney <alcarneyme@gmail.com>
2023-06-19 19:42:21 +0100

    code: Basic re-implementation of previews
    This does just enough to open a preview pane for the current document
    on demand, it even works for multi-root/multi-project situations -
    though currently only one project active at a time.
    
    Fancier features like synced scrolling/document switching etc
    will (hopefully!) follow soon

---
+++ code/src/common/constants.ts
@@ -0,0 +1,5 @@
+export namespace Commands {
+  export const OPEN_PREVIEW = "esbonio.preview.open"
+  export const OPEN_PREVIEW_TO_SIDE = "esbonio.preview.openSide"
+  export const PREVIEW_FILE = "esbonio.server.previewFile"
+}
