706e9e8444cf3b7b8b490697c6515d64f68e8681
Alex Carney <alcarneyme@gmail.com>
2023-06-29 19:46:36 +0100

    code: Implement restart server command

--- code/src/common/constants.ts
+++ code/src/common/constants.ts
@@ -2,4 +2,7 @@ export namespace Commands {
   export const OPEN_PREVIEW = "esbonio.preview.open"
   export const OPEN_PREVIEW_TO_SIDE = "esbonio.preview.openSide"
   export const PREVIEW_FILE = "esbonio.server.previewFile"
+
+  export const RESTART_SERVER = "esbonio.server.restart"
+}
 }
