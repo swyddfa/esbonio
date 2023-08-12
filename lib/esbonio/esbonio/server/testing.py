ac036e7285c74e0828294be3a54153cdeeaa2123
Alex Carney <alcarneyme@gmail.com>
2023-08-12 19:46:31 +0100

    lsp: Add `esbono.server.testing`
    This is to avoid dependency issues when trying to import from
    `esbonio.lsp.testing`

---
+++ lib/esbonio/esbonio/server/testing.py
@@ -0,0 +1,14 @@
+"""Helpers and utilities for writing tests."""
+from lsprotocol import types
+
+
+def range_from_str(spec: str) -> types.Range:
+    """Create a range from the given string ``a:b-x:y``"""
+    start, end = spec.split("-")
+    sl, sc = start.split(":")
+    el, ec = end.split(":")
+
+    return types.Range(
+        start=types.Position(line=int(sl), character=int(sc)),
+        end=types.Position(line=int(el), character=int(ec)),
+    )
