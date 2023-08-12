2dca4d037ad3cc1a0da1aa1897dc00426856b969
Alex Carney <alcarneyme@gmail.com>
2023-08-12 19:53:36 +0100

    lsp: Change default startup module

--- lib/esbonio/esbonio/__main__.py
+++ lib/esbonio/esbonio/__main__.py
@@ -1,18 +1,7 @@
-"""Default entry point, identical to calling ``python -m esbonio.lsp.sphinx``"""
+"""Default startup module, identical to calling ``python -m esbonio.server``"""
 import sys
 
-from esbonio import cli
-from esbonio.lsp.sphinx import DEFAULT_MODULES
-from esbonio.lsp.sphinx import SphinxLanguageServer
-
-_cli = cli.setup_cli("esbonio", "Esbonio's Sphinx language server.")
-_cli.set_defaults(modules=DEFAULT_MODULES)
-_cli.set_defaults(server_cls=SphinxLanguageServer)
-
-
-def main():
-    cli.main(_cli)
-
+from esbonio.server.cli import main
 
 if __name__ == "__main__":
     sys.exit(main())
