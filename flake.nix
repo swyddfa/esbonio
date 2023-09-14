57b3eed0c43061130b32d96a98499d000044b199
Alex Carney <alcarneyme@gmail.com>
2023-09-14 10:50:26 +0100

    nix: Update nvim-lspconfig example config
    - Implement sync scrolling
    - Attempt to automatically detect the user's virtual env
    - Move nix config code into its own package - simplifying the
      top-level flake.

--- flake.nix
+++ flake.nix
@@ -3,46 +3,32 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    esbonio.url = "path:lib/esbonio";
-    esbonio.inputs.nixpkgs.follows = "nixpkgs";
     utils.url = "github:numtide/flake-utils";
+
+    lsp-devtools.url = "github:swyddfa/lsp-devtools";
+    lsp-devtools.inputs.nixpkgs.follows = "nixpkgs";
+    lsp-devtools.inputs.utils.follows = "utils";
   };
 
-  outputs = { self, nixpkgs, esbonio, utils }:
+  outputs = { self, nixpkgs, lsp-devtools, utils }:
 
     utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system ; overlays = [ esbonio.overlays.default ];};
+        pkgs = import nixpkgs { inherit system ; overlays = [ self.overlays.default ];};
 
-        esbonio-lsp = pkgs.python3Packages.esbonio.overridePythonAttrs (_: { doCheck = false ; });
-        paths = pkgs.lib.makeBinPath [
-          pkgs.neovim
-          esbonio-lsp
-        ];
+        nvim-lspconfig = pkgs.callPackage ./docs/lsp/editors/nvim-lspconfig {};
 
-        pluginList = with pkgs.vimPlugins; [
-          nvim-lspconfig
-        ];
-        plugins = pkgs.stdenv.mkDerivation {
-          name = "esbonio-nvim-plugins";
-          buildCommand = ''
-            mkdir -p $out/nvim/site/pack/plugins/start/
-            ${pkgs.lib.concatMapStringsSep "\n" (path: "ln -s ${path} $out/nvim/site/pack/plugins/start/")  pluginList }
-          '';
-        };
-
-        initVim = builtins.readFile ./docs/lsp/editors/nvim-lspconfig/init.vim;
-        neovim = pkgs.writeShellScriptBin "nvim" ''
-          export PATH=${paths}:$PATH
-          export XDG_CONFIG_DIRS=
-          export XDG_DATA_DIRS=${plugins.outPath}
-          nvim --clean --cmd 'source ${pkgs.writeText "init.vim" initVim}' "$@"
-        '';
       in {
-         apps.default = {
+         apps.nvim-lspconfig = {
            type = "app";
-           program = "${neovim}/bin/nvim";
+           program = "${nvim-lspconfig}/bin/nvim";
          };
-       }
-    );
+
+      }) // {
+        overlays.default = self: super:
+          nixpkgs.lib.composeManyExtensions [
+            lsp-devtools.overlays.default
+            (import ./lib/esbonio/nix/esbonio-overlay.nix)
+          ] self super;
+      };
 }
