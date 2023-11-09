a6b47e04ce99afdff3bc5b826fbfc65cd73ca3b4
Alex Carney <alcarneyme@gmail.com>
2023-11-09 22:32:23 +0000

    lsp: Add dedicated configuration component
    The idea is that all configuration will be managed in a centralised
    location. The `WorkspaceConfiguration` component will be attached to the
    main language server will be responsible from merging configuration
    values from a number of sources.
    
    - Initialization options
    - ``workspace/configuration`` requests
    - Configuration files
    
    While this is not implemented yet, this component will also make sure
    that the configuration values are kept up to date. Eventually features
    will be able to subscribe to changes in the sections that they are
    interested in.

---
+++ lib/esbonio/esbonio/server/_configuration.py
@@ -0,0 +1,198 @@
+from __future__ import annotations
+
+import json
+import typing
+from typing import Any
+from typing import Dict
+from typing import List
+from typing import Optional
+from typing import Set
+from typing import Type
+from typing import TypeVar
+
+import attrs
+
+from ._uri import Uri
+
+if typing.TYPE_CHECKING:
+    from .server import EsbonioLanguageServer
+
+T = TypeVar("T")
+
+
+class WorkspaceConfiguration:
+    """Manages the configuration values for the server.
+
+    This will looks configuration in the following locations, in descending order of
+    priority.
+
+    - The server's ``initializationOptions``
+    - ``workspace/configuration`` requests
+    - A relevant configuration file
+
+    **Note:** This assumes that a user's client is *either* going to be using
+    ``initializationOptions`` *or* ``workspace/configuration`` not both. Any wierdness
+    caused by combining both will (probably) not be fixed.
+
+    **Note:** ``initializationOptions`` will implictly work at the global scope. There
+    will be not multi-root/multi-project support using ``initializationOptions``. Use
+    ``workspace/configuration`` or config files instead.
+    """
+
+    def __init__(self, server: EsbonioLanguageServer):
+        self.server = server
+        """The language server"""
+
+        self.logger = server.logger.getChild("Configuration")
+        """The logger instance to use"""
+
+        self._initialization_options: Dict[str, Any] = {}
+        """The received initializaion options (if any)"""
+
+        self._workspace_config: Dict[str, Dict[str, Any]] = {}
+        """The cached workspace configuration."""
+
+        self._file_config: Dict[str, Dict[str, Any]] = {}
+        """The cached configuration coming from configuration files."""
+
+    @property
+    def initialization_options(self):
+        return self._initialization_options
+
+    @initialization_options.setter
+    def initialization_options(self, value):
+        """Ensure the init options are namespaced."""
+        if value is None or value == {}:
+            return
+
+        if "esbonio" not in value:
+            value = dict(esbonio=value)
+
+        self._initialization_options = value
+
+    @property
+    def converter(self):
+        return self.server.converter
+
+    def get(
+        self, section: str, spec: Type[T], scope: Optional[Uri] = None
+    ) -> Optional[T]:
+        """Get the requested configuration section.
+
+        Parameters
+        ----------
+        section
+           The configuration section to retrieve
+
+        spec
+           A type representing the expected "shape" of the configuration section
+
+        scope
+           An optional uri, specifying the scope in which to lookup the configuration.
+
+        Returns
+        -------
+        T | None
+           The requested configuration section, if available, parsed as an instance of
+           ``T``. ``None``, otherwise
+        """
+        file_scope = self._uri_to_file_scope(scope)
+        self.logger.debug("File scope: '%s'", file_scope)
+
+        workspace_scope = self._uri_to_workspace_scope(scope)
+        self.logger.debug("Workspace scope: '%s'", workspace_scope)
+
+        # To keep things simple, this method assumes that all available config is already
+        # cached locally. Populating the cache is handled elsewhere.
+        file_config = self._file_config.get(file_scope, {})
+        workspace_config = self._workspace_config.get(workspace_scope, {})
+
+        # Combine and resolve all the config sources - order matters!
+        config = _merge_configs(
+            file_config, workspace_config, self._initialization_options
+        )
+        self.logger.debug("Resolved config: %s", json.dumps(config, indent=2))
+
+        # Extract the requested section.
+        config_section = config
+        for name in section.split("."):
+            config_section = config_section.get(name, {})
+
+        self.logger.debug("Config section: %s", json.dumps(config_section, indent=2))
+
+        try:
+            return self.converter.structure(config_section, spec)
+        except Exception:
+            self.logger.error(
+                "Unable to parse configuation as '%s', using defaults",
+                spec.__name__,
+                exc_info=True,
+            )
+            return spec()
+
+    def _uri_to_file_scope(self, uri: Optional[Uri]) -> str:
+        folder_uris = list(self._file_config.keys())
+        return _uri_to_scope(folder_uris, uri)
+
+    def _uri_to_workspace_scope(self, uri: Optional[Uri]) -> str:
+        folder_uris = [f.uri for f in self.server.workspace.folders.values()]
+
+        if (root_uri := self.server.workspace.root_uri) is not None:
+            folder_uris.append(root_uri)
+
+        return _uri_to_scope(folder_uris, uri)
+
+def _uri_to_scope(known_scopes: List[str], uri: Optional[Uri]) -> str:
+    """Convert the given uri to a scope or the empty string if none could be found.
+
+    Parameters
+    ----------
+    known_scopes
+       The collection of known scopes.
+
+    uri
+       The uri to convert
+
+    Returns
+    -------
+    str
+        The scope to use
+    """
+    if uri is None:
+        return ""
+
+    uri = uri.resolve()
+    known_uris = [str(Uri.parse(u).resolve()) for u in known_scopes]
+
+    candidates = [scope for scope in known_uris if str(uri).startswith(scope)]
+    if len(candidates) == 0:
+        return ""
+
+    # Return the most specific match.
+    return sorted(candidates, key=len, reverse=True)[0]
+
+
+def _merge_configs(*configs: Dict[str, Any]):
+    """Recursively merge all the given configuration sources together.
+
+    The last config given takes precedence.
+    """
+    final = {}
+    all_keys: Set[str] = set()
+
+    for c in configs:
+        all_keys.update(c.keys())
+
+    for k in all_keys:
+        values = [
+            v for c in configs if (v := c.get(k, attrs.NOTHING)) is not attrs.NOTHING
+        ]
+
+        # Do we need to recurse?
+        if isinstance(values[-1], dict):
+            # Be sure to only pass dictionary values to _merge_configs.
+            final[k] = _merge_configs(*[v for v in values if isinstance(v, dict)])
+        else:
+            final[k] = values[-1]
+
+    return final
---
+++ lib/esbonio/tests/server/test_configuration.py
@@ -0,0 +1,334 @@
+import itertools
+from typing import Any
+from typing import Dict
+from typing import List
+from typing import Optional
+from typing import Type
+from typing import TypeVar
+
+import attrs
+import pytest
+from lsprotocol import types
+from pygls import IS_WIN
+from pygls.workspace import Workspace
+
+from esbonio.server import EsbonioLanguageServer
+from esbonio.server import Uri
+from esbonio.server._configuration import _merge_configs
+from esbonio.server._configuration import _uri_to_scope
+
+T = TypeVar("T")
+
+
+@attrs.define
+class ExampleConfig:
+    log_level: str = "error"
+    log_names: List[str] = attrs.field(factory=list)
+
+
+@pytest.fixture()
+def server(event_loop):
+    """Return a server instance for testing."""
+    _server = EsbonioLanguageServer(loop=event_loop)
+    return _server
+
+
+@pytest.mark.parametrize(
+    "init_options,workspace_config,file_config,section,spec,scope,expected",
+    [
+        pytest.param(  # When there's no configuration at all, just return the defaults.
+            {},
+            {},
+            {},
+            "esbonio.server",
+            ExampleConfig,
+            None,
+            ExampleConfig(log_level="error", log_names=[]),
+            id="no-config",
+        ),
+        pytest.param(
+            dict(esbonio=dict(server=dict(logLevel="info"))),
+            {},
+            {},
+            "esbonio.server",
+            ExampleConfig,
+            None,
+            ExampleConfig(log_level="info", log_names=[]),
+            id="init-options",
+        ),
+        pytest.param(  # Handle the case where the client omits the top-level namespace
+            dict(server=dict(logLevel="info")),
+            {},
+            {},
+            "esbonio.server",
+            ExampleConfig,
+            None,
+            ExampleConfig(log_level="info", log_names=[]),
+            id="init-options-no-namespace",
+        ),
+        pytest.param(
+            {},
+            {"": dict(esbonio=dict(server=dict(logLevel="info")))},
+            {},
+            "esbonio.server",
+            ExampleConfig,
+            None,
+            ExampleConfig(log_level="info", log_names=[]),
+            id="global-workspace-config",
+        ),
+        pytest.param(
+            {},
+            {
+                "file:///path/to/workspace": dict(
+                    esbonio=dict(server=dict(logLevel="info"))
+                )
+            },
+            {},
+            "esbonio.server",
+            ExampleConfig,
+            "file:///path/to/workspace/file.txt",
+            ExampleConfig(log_level="info", log_names=[]),
+            id="workspace-config",
+        ),
+        pytest.param(  # Handle the case where the scope does not match a known workspace
+            {},
+            {
+                "file:///path/to/workspace": dict(
+                    esbonio=dict(server=dict(logLevel="info"))
+                )
+            },
+            {},
+            "esbonio.server",
+            ExampleConfig,
+            "file:///path/to/other/workspace/file.txt",
+            ExampleConfig(log_level="error", log_names=[]),
+            id="workspace-config-scope-mismatch",
+        ),
+        pytest.param(  # Handle the case where the scope does not match a known file
+            {},
+            {},
+            {
+                "file:///path/to/workspace/docs": dict(
+                    esbonio=dict(server=dict(logLevel="info"))
+                )
+            },
+            "esbonio.server",
+            ExampleConfig,
+            "file:///path/to/workspace/docs/file.txt",
+            ExampleConfig(log_level="info", log_names=[]),
+            id="file-config",
+        ),
+        pytest.param(  # Handle the case where the scope does not match a known file
+            {},
+            {},
+            {
+                "file:///path/to/workspace/docs": dict(
+                    esbonio=dict(server=dict(logLevel="info"))
+                )
+            },
+            "esbonio.server",
+            ExampleConfig,
+            "file:///path/to/workspace/workspace/file.txt",
+            ExampleConfig(log_level="error", log_names=[]),
+            id="file-config-scope-mismatch",
+        ),
+        pytest.param(  # Allow the client to override the file based config
+            {},
+            {
+                "file:///path/to/workspace": dict(
+                    esbonio=dict(server=dict(logLevel="debug"))
+                )
+            },
+            {
+                "file:///path/to/workspace/docs": dict(
+                    esbonio=dict(server=dict(logLevel="info", logNames=["file"]))
+                )
+            },
+            "esbonio.server",
+            ExampleConfig,
+            "file:///path/to/workspace/docs/file.txt",
+            ExampleConfig(log_level="debug", log_names=["file"]),
+            id="workspace-file-override",
+        ),
+    ],
+)
+def test_get_configuration(
+    server: EsbonioLanguageServer,
+    init_options: Dict[str, Any],
+    workspace_config: Dict[str, Any],
+    file_config: Dict[str, Any],
+    section: str,
+    spec: Type[T],
+    scope: Optional[str],
+    expected: T,
+):
+    """Ensure that we can get configuration values correctly.
+
+    The number of parameters to this test case is a lot! But the test itself is fairly
+    simple. Given the current configuration of the server, can we resolve the
+    configuration correctly and return the expected set of values?
+
+    Parameters
+    ----------
+    init_options
+       The server's initialization options.
+
+    workspace_config
+       The server's cached ``workspace/configuration`` requests
+
+    file_config
+       The server's cached config coming from configuration files
+
+    section
+       The configuration section to retrieve
+
+    spec
+       The datatype describing the configuration
+
+    scope
+       The scope at which to get the configuration
+
+    expected
+       The expected value.
+    """
+    server.configuration.initialization_options = init_options
+    server.configuration._workspace_config = workspace_config
+    server.configuration._file_config = file_config
+
+    # Infer the workspace based on the given config.
+    workspace_folders = [
+        types.WorkspaceFolder(uri=uri, name=f"workspace-{idx}")
+        for idx, uri in enumerate(workspace_config.keys())
+        if uri != ""
+    ]
+    server.lsp._workspace = Workspace(None, workspace_folders=workspace_folders)
+
+    scope_uri = Uri.parse(scope) if scope else None
+
+    actual = server.configuration.get(section, spec, scope=scope_uri)
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "known_scopes,setup",
+    [
+        *itertools.product(
+            [
+                ["file:///path/to/test"],
+            ],
+            [
+                ("file:///path/to/test/file.txt", "file:///path/to/test"),
+                ("file:///path/to/other/file.txt", ""),
+                (None, ""),
+            ],
+        ),
+        *itertools.product(
+            [
+                ["file:///path/to/test", "file:///path/to/test/sub/folder"],
+            ],
+            [
+                ("file:///path/to/test/file.txt", "file:///path/to/test"),
+                ("file:///path/to/test/sub/file.txt", "file:///path/to/test"),
+                (
+                    "file:///path/to/test/sub/folder/file.txt",
+                    "file:///path/to/test/sub/folder",
+                ),
+            ],
+        ),
+    ],
+)
+def test_uri_to_scope(known_scopes, setup):
+    """Ensure that we can determine the correct scope for the given uri."""
+
+    uri, expected = setup
+    parsed_uri = Uri.parse(uri) if uri else None
+    scope = _uri_to_scope(known_scopes, parsed_uri)
+    assert scope == expected
+
+
+@pytest.mark.parametrize(
+    "known_scopes,setup",
+    [
+        *itertools.product(
+            [
+                ["file:///c:/path/to/test", "file:///c:/path/to/test/sub/folder"],
+            ],
+            [
+                ("file:///c:/path/to/test/file.txt", "file:///c:/path/to/test"),
+                ("file:///c%3A/path/to/test/file.txt", "file:///c:/path/to/test"),
+                ("file:///C:/path/to/test/file.txt", "file:///c:/path/to/test"),
+                ("file:///C%3A/path/to/test/file.txt", "file:///c:/path/to/test"),
+                ("file:///c:/path/to/test/sub/file.txt", "file:///c:/path/to/test"),
+                (
+                    "file:///c:/path/to/test/sub/folder/file.txt",
+                    "file:///c:/path/to/test/sub/folder",
+                ),
+            ],
+        ),
+    ],
+)
+@pytest.mark.skipif(not IS_WIN, reason="windows only")
+def test_uri_to_scope_windows(known_scopes, setup):
+    """Ensure that we can determine the correct scope for the given uri."""
+
+    uri, expected = setup
+    scope = _uri_to_scope(known_scopes, Uri.parse(uri))
+    assert scope == expected
+
+
+@pytest.mark.parametrize(
+    "configs, expected",
+    [
+        (  # Last value wins
+            [
+                {"a": 1},
+                {"a": 3},
+                {"a": 2},
+            ],
+            {"a": 2},
+        ),
+        (  # None is a valid value
+            [
+                {"a": 1},
+                {"a": 2},
+                {"a": None},
+            ],
+            {"a": None},
+        ),
+        (  # Simple merge
+            [
+                {"a": 1},
+                {"b": 3},
+                {"c": 2},
+            ],
+            {"a": 1, "b": 3, "c": 2},
+        ),
+        (  # Ensure nested values are merged as well
+            [
+                {"a": 1, "b": {"c": 1}},
+                {"b": {"c": 4}},
+                {"b": {"d": 7}},
+            ],
+            {"a": 1, "b": {"c": 4, "d": 7}},
+        ),
+        (  # Handle the case where there's a type mismatch.
+            [
+                {"a": {"c": 1}},
+                {"a": 2},
+                {"a": {"d": 7}},
+            ],
+            {"a": {"c": 1, "d": 7}},
+        ),
+        (  # Let the final config dictate the type of a given field.
+            [
+                {"a": {"c": 1}},
+                {"a": {"d": 7}},
+                {"a": 2},
+            ],
+            {"a": 2},
+        ),
+    ],
+)
+def test_merge_configs(configs, expected):
+    """Ensure that we can merge configurations together correctly."""
+    assert expected == _merge_configs(*configs)
