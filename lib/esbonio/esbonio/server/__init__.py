9ad405c630f7f9a346141706fc08c46304ec3968
Alex Carney <alcarneyme@gmail.com>
2023-12-23 16:48:54 +0000

    sphinx-agent: Move regexp patterns to `sphinx_agent.types`
    This allows them to be reused in both the server and sphinx agent,
    without introducing any import issues

--- lib/esbonio/esbonio/server/__init__.py
+++ lib/esbonio/esbonio/server/__init__.py
@@ -1,16 +1,12 @@
 from ._log import LOG_NAMESPACE
 from ._log import MemoryHandler
-from ._patterns import DIRECTIVE
-from ._patterns import ROLE
 from ._uri import Uri
 from .feature import LanguageFeature
 from .server import EsbonioLanguageServer
 from .server import EsbonioWorkspace
 
 __all__ = [
-    "DIRECTIVE",
     "LOG_NAMESPACE",
-    "ROLE",
     "EsbonioLanguageServer",
     "EsbonioWorkspace",
     "LanguageFeature",
