c7a1a4a904d01fff808b3eb3d5062d844d4622f2
Alex Carney <alcarneyme@gmail.com>
2024-01-13 20:16:54 +0000

    lsp: Implement support for the `.. default-domain::` directive

--- lib/esbonio/esbonio/server/features/sphinx_support/directives.py
+++ lib/esbonio/esbonio/server/features/sphinx_support/directives.py
@@ -1,9 +1,16 @@
-from typing import List
-from typing import Optional
+from __future__ import annotations
+
+import typing
+
+from lsprotocol import types
 
 from esbonio import server
 from esbonio.server.features import directives
 from esbonio.server.features.sphinx_manager import SphinxManager
+
+if typing.TYPE_CHECKING:
+    from typing import List
+    from typing import Optional
 
 
 class SphinxDirectives(directives.DirectiveProvider):
@@ -20,14 +27,25 @@ class SphinxDirectives(directives.DirectiveProvider):
         if (client := await self.manager.get_client(context.uri)) is None:
             return None
 
-        # TODO .. default-domain:: support
+        # Does the document have a default domain set?
+        results = await client.find_symbols(
+            uri=str(context.uri.resolve()),
+            kind=types.SymbolKind.Class.value,
+            detail="default-domain",
+        )
+        if len(results) > 0:
+            default_domain = results[0][1]
+        else:
+            default_domain = None
+
         primary_domain = await client.get_config_value("primary_domain")
+        active_domain = default_domain or primary_domain or "py"
 
         result: List[directives.Directive] = []
         for name, implementation in await client.get_directives():
-            # Also suggest unqualified versions of directives from the primary_domain.
-            if name.startswith(f"{primary_domain}:"):
-                short_name = name.replace(f"{primary_domain}:", "")
+            # Also suggest unqualified versions of directives from the currently active domain.
+            if name.startswith(f"{active_domain}:"):
+                short_name = name.replace(f"{active_domain}:", "")
                 result.append(
                     directives.Directive(name=short_name, implementation=implementation)
                 )
