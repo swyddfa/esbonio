353fae95f0faddc4062d646616b094c1f47043e6
Frederic Brin <frederic.brin@exane.com>
2024-01-13 14:29:18 +0100

    Fix path seperator in var
    ":" on Unix vs ";" on Windows

--- lib/esbonio/esbonio/server/features/sphinx_manager/client_subprocess.py
+++ lib/esbonio/esbonio/server/features/sphinx_manager/client_subprocess.py
@@ -346,7 +346,7 @@ def make_test_sphinx_client() -> SubprocessSphinxClient:
 
 def get_sphinx_env(config: SphinxConfig) -> Dict[str, str]:
     """Return the set of environment variables to use with the Sphinx process."""
-    env = {"PYTHONPATH": ":".join([str(p) for p in config.python_path])}
+    env = {"PYTHONPATH": os.pathsep.join([str(p) for p in config.python_path])}
 
     passthrough = set(config.env_passthrough)
     if IS_WIN and "SYSTEMROOT" not in passthrough:
