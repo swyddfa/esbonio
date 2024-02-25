5b1a898c77aa867f4309752801bece9791accf96
Alex Carney <alcarneyme@gmail.com>
2024-02-25 20:08:15 +0000

    lsp: Delete `MockSphinxClient`
    The tests now use the real thing and there does not seem to be much
    benefit in trying to maintain this.

--- lib/esbonio/esbonio/server/features/sphinx_manager/__init__.py
+++ lib/esbonio/esbonio/server/features/sphinx_manager/__init__.py
@@ -4,8 +4,6 @@ from esbonio.server import EsbonioLanguageServer
 
 from .client import ClientState
 from .client import SphinxClient
-from .client_mock import MockSphinxClient
-from .client_mock import mock_sphinx_client_factory
 from .client_subprocess import make_subprocess_sphinx_client
 from .config import SphinxConfig
 from .manager import SphinxManager
@@ -15,8 +13,6 @@ __all__ = [
     "SphinxClient",
     "SphinxConfig",
     "SphinxManager",
-    "MockSphinxClient",
-    "mock_sphinx_client_factory",
 ]
 
 
