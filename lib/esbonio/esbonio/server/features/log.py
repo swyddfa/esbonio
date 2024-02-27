12c288fa5212f0d5a48fb98b1e3cd32cc61a5791
Alex Carney <alcarneyme@gmail.com>
2024-02-27 20:21:17 +0000

    lsp: Remove diagnostic code
    It's not currently used and can be brought back as a filter if required

--- lib/esbonio/esbonio/server/features/log.py
+++ lib/esbonio/esbonio/server/features/log.py
@@ -1,18 +1,13 @@
 import enum
 import json
 import logging
-import pathlib
-import re
 import textwrap
-import traceback
 from typing import List
-from typing import Tuple
 
 import attrs
 from lsprotocol import types
 
 from esbonio import server
-from esbonio.server import Uri
 
 LOG_LEVELS = {
     "debug": logging.DEBUG,
@@ -21,25 +16,6 @@ LOG_LEVELS = {
 }
 
 
-# e.g. /.../filename.rst:54: (ERROR/3) Unexpected indentation.
-#      c:\...\filename.rst:54: (ERROR/3) Unexpected indentation.
-DOCUTILS_ERROR = re.compile(
-    r"""
-    ^\s*(?P<filepath>.+?):
-    (?P<linum>\d+):
-    \s*\((?P<levelname>\w+)/(?P<levelnum>\d)\)
-    (?P<message>.*)$
-    """,
-    re.VERBOSE,
-)
-
-DOCUTILS_SEVERITY = {
-    0: types.DiagnosticSeverity.Hint,
-    1: types.DiagnosticSeverity.Information,
-    2: types.DiagnosticSeverity.Warning,
-    3: types.DiagnosticSeverity.Error,
-    4: types.DiagnosticSeverity.Error,
-}
 
 
 class LogFilter(logging.Filter):
@@ -62,104 +38,13 @@ class LspHandler(logging.Handler):
     ):
         super().__init__()
         self.server = server
-        self.show_deprecation_warnings = show_deprecation_warnings
-
-    def get_warning_path(self, warning: str) -> Tuple[str, List[str]]:
-        """Determine the filepath that the warning was emitted from."""
-
-        path, *parts = warning.split(":")
-
-        # On windows the rest of the path will be in the first element of parts.
-        if pathlib.Path(warning).drive:
-            path += f":{parts.pop(0)}"
-
-        return path, parts
-
-    def handle_warning(self, record: logging.LogRecord):
-        """Publish warnings to the client as diagnostics."""
-
-        if not isinstance(record.args, tuple):
-            self.server.logger.debug(
-                "Unable to handle warning, expected tuple got: %s", record.args
-            )
-            return
-
-        # The way warnings are logged is different in Python 3.11+
-        if len(record.args) == 0:
-            argument = record.msg
-        else:
-            argument = record.args[0]  # type: ignore
-
-        if not isinstance(argument, str):
-            self.server.logger.debug(
-                "Unable to handle warning, expected string got: %s", argument
-            )
-            return
-
-        warning, *_ = argument.split("\n")
-        path, (linenum, category, *msg) = self.get_warning_path(warning)
-
-        category = category.strip()
-        message = ":".join(msg).strip()
-
-        try:
-            line = int(linenum)
-        except ValueError:
-            line = 1
-            self.server.logger.debug(
-                "Unable to parse line number: '%s'\n%s", linenum, traceback.format_exc()
-            )
-
-        tags = []
-        if category == "DeprecationWarning":
-            tags.append(types.DiagnosticTag.Deprecated)
-
-        diagnostic = types.Diagnostic(
-            range=types.Range(
-                start=types.Position(line=line - 1, character=0),
-                end=types.Position(line=line, character=0),
-            ),
-            message=message,
-            severity=types.DiagnosticSeverity.Warning,
-            tags=tags,
-        )
-
-        self.server.add_diagnostics("esbonio", Uri.for_file(path), diagnostic)
-        self.server.sync_diagnostics()
-
-    def handle_diagnostic(self, record: logging.LogRecord):
-        """Look for any diagnostics to report in the log message."""
-
-        if (match := DOCUTILS_ERROR.match(record.msg)) is not None:
-            uri = Uri.for_file(match.group("filepath"))
-            line = int(match.group("linum"))
-            severity = int(match.group("levelnum"))
-
-            diagnostic = types.Diagnostic(
-                message=match.group("message").strip(),
-                severity=DOCUTILS_SEVERITY.get(severity),
-                range=types.Range(
-                    start=types.Position(line=line - 1, character=0),
-                    end=types.Position(line=line, character=0),
-                ),
-            )
-            self.server.add_diagnostics("docutils", uri, diagnostic)
 
     def emit(self, record: logging.LogRecord) -> None:
         """Sends the record to the client."""
 
-        # To avoid infinite recursions, it's simpler to just ignore all log records
-        # coming from pygls...
+        # To avoid infinite recursions, we can't process records coming from pygls.
         if "pygls" in record.name:
             return
-
-        if record.name == "py.warnings":
-            if not self.show_deprecation_warnings:
-                return
-
-            self.handle_warning(record)
-        else:
-            self.handle_diagnostic(record)
 
         log = self.format(record).strip()
         self.server.show_message_log(log)
