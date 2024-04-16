8db4fd4af51ca0d58fac122072c9551034fd8bbc
Alex Carney <alcarneyme@gmail.com>
2024-04-16 20:14:40 +0100

    sphinx-agent: Override Sphinx's logging setup... again!
    It turns out we still need to override the logging setup - so that we
    can inject our `DiagnosticFilter`. However, this time around our
    overriden setup is *much* less invasive.

--- lib/esbonio/esbonio/sphinx_agent/app.py
+++ lib/esbonio/esbonio/sphinx_agent/app.py
@@ -1,12 +1,30 @@
 from __future__ import annotations
 
+import logging
 import pathlib
+from typing import IO
 
 from sphinx.application import Sphinx as _Sphinx
 from sphinx.util import console
+from sphinx.util import logging as sphinx_logging_module
+from sphinx.util.logging import NAMESPACE as SPHINX_LOG_NAMESPACE
 
 from .database import Database
 from .log import DiagnosticFilter
+
+sphinx_logger = logging.getLogger(SPHINX_LOG_NAMESPACE)
+sphinx_log_setup = sphinx_logging_module.setup
+
+
+def setup_logging(app: Sphinx, status: IO, warning: IO):
+
+    # Run the usual setup
+    sphinx_log_setup(app, status, warning)
+
+    # Attach our diagnostic filter to the warning handler.
+    for handler in sphinx_logger.handlers:
+        if handler.level == logging.WARNING:
+            handler.addFilter(app.esbonio.log)
 
 
 class Esbonio:
@@ -19,6 +37,9 @@ class Esbonio:
     def __init__(self, dbpath: pathlib.Path, app: _Sphinx):
         self.db = Database(dbpath)
         self.log = DiagnosticFilter(app)
+
+        # Override sphinx's usual logging setup function
+        sphinx_logging_module.setup = setup_logging  # type: ignore
 
 
 class Sphinx(_Sphinx):
