622ed345c61b4c29428366c94b870b17cd69a770
Alex Carney <alcarneyme@gmail.com>
2024-05-26 19:43:49 +0100

    lsp: Implement the `objects` target provider
    This implements the `objects` target provider which can provide
    completion suggestions for Sphinx domain objects.

--- lib/esbonio/esbonio/server/features/sphinx_support/roles.py
+++ lib/esbonio/esbonio/server/features/sphinx_support/roles.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import logging
 import typing
 
 from lsprotocol import types as lsp
@@ -13,6 +14,64 @@ if typing.TYPE_CHECKING:
     from typing import List
     from typing import Optional
 
+    from esbonio.server import Uri
+    from esbonio.server.features.project_manager import Project
+
+
+TARGET_KINDS = {
+    "attribute": lsp.CompletionItemKind.Field,
+    "doc": lsp.CompletionItemKind.File,
+    "class": lsp.CompletionItemKind.Class,
+    "envvar": lsp.CompletionItemKind.Variable,
+    "function": lsp.CompletionItemKind.Function,
+    "method": lsp.CompletionItemKind.Method,
+    "module": lsp.CompletionItemKind.Module,
+    "term": lsp.CompletionItemKind.Text,
+}
+
+
+class ObjectsProvider(roles.RoleTargetProvider):
+    """Expose domain objects as potential role targets"""
+
+    def __init__(self, logger: logging.Logger, manager: ProjectManager):
+        self.manager = manager
+        self.logger = logger
+
+    async def suggest_targets(  # type: ignore[override]
+        self,
+        context: server.CompletionContext,
+        *,
+        obj_types: List[str],
+    ) -> Optional[List[lsp.CompletionItem]]:
+        self.logger.debug("Suggesting targets for types: %s", obj_types)
+
+        if (project := self.manager.get_project(context.uri)) is None:
+            return None
+
+        db = await project.get_db()
+        query = " ".join(
+            [
+                "SELECT name, display, objtype FROM objects",
+                "WHERE printf('%s:%s', domain, objtype) IN (",
+                ", ".join("?" for _ in range(len(obj_types))),
+                ");",
+            ]
+        )
+
+        items = []
+        cursor = await db.execute(query, tuple(obj_types))
+        for name, display, type_ in await cursor.fetchall():
+            kind = TARGET_KINDS.get(type_, lsp.CompletionItemKind.Reference)
+            items.append(
+                lsp.CompletionItem(
+                    label=name,
+                    detail=display,
+                    kind=kind,
+                ),
+            )
+
+        return items
+
 
 class SphinxRoles(roles.RoleProvider):
     """Support for roles in a sphinx project."""
@@ -20,17 +79,12 @@ class SphinxRoles(roles.RoleProvider):
     def __init__(self, manager: ProjectManager):
         self.manager = manager
 
-    async def suggest_roles(
-        self, context: server.CompletionContext
-    ) -> Optional[List[types.Role]]:
-        """Given a completion context, suggest roles that may be used."""
-
-        if (project := self.manager.get_project(context.uri)) is None:
-            return None
+    async def get_default_domain(self, project: Project, uri: Uri) -> str:
+        """Get the name of the default domain for the given document"""
 
         # Does the document have a default domain set?
         results = await project.find_symbols(
-            uri=str(context.uri.resolve()),
+            uri=str(uri.resolve()),
             kind=lsp.SymbolKind.Class.value,
             detail="default-domain",
         )
@@ -40,7 +94,32 @@ class SphinxRoles(roles.RoleProvider):
             default_domain = None
 
         primary_domain = await project.get_config_value("primary_domain")
-        active_domain = default_domain or primary_domain or "py"
+        return default_domain or primary_domain or "py"
+
+    async def get_role(self, uri: Uri, name: str) -> Optional[types.Role]:
+        """Return the role with the given name."""
+
+        if (project := self.manager.get_project(uri)) is None:
+            return None
+
+        if (role := await project.get_role(name)) is not None:
+            return role
+
+        if (role := await project.get_role(f"std:{name}")) is not None:
+            return role
+
+        default_domain = await self.get_default_domain(project, uri)
+        return await project.get_role(f"{default_domain}:{name}")
+
+    async def suggest_roles(
+        self, context: server.CompletionContext
+    ) -> Optional[List[types.Role]]:
+        """Given a completion context, suggest roles that may be used."""
+
+        if (project := self.manager.get_project(context.uri)) is None:
+            return None
+
+        default_domain = await self.get_default_domain(project, context.uri)
 
         result: List[types.Role] = []
         for name, implementation in await project.get_roles():
@@ -52,8 +131,8 @@ class SphinxRoles(roles.RoleProvider):
                 )
 
             # Also suggest unqualified versions of directives from the currently active domain.
-            elif name.startswith(f"{active_domain}:"):
-                short_name = name.replace(f"{active_domain}:", "")
+            elif name.startswith(f"{default_domain}:"):
+                short_name = name.replace(f"{default_domain}:", "")
                 result.append(
                     types.Role(name=short_name, implementation=implementation)
                 )
@@ -64,8 +143,14 @@ class SphinxRoles(roles.RoleProvider):
 
 
 def esbonio_setup(
+    esbonio: server.EsbonioLanguageServer,
     project_manager: ProjectManager,
     roles_feature: roles.RolesFeature,
 ):
-    provider = SphinxRoles(project_manager)
-    roles_feature.add_provider(provider)
+    role_provider = SphinxRoles(project_manager)
+    obj_provider = ObjectsProvider(
+        esbonio.logger.getChild("ObjectsProvider"), project_manager
+    )
+
+    roles_feature.add_role_provider(role_provider)
+    roles_feature.add_target_provider("objects", obj_provider)
