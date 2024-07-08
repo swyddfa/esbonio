280f66683f67ceedffe1e9744919d8ab226e80b3
Alex Carney <alcarneyme@gmail.com>
2024-07-08 20:32:21 +0100

    sphinx-agent: Inject uri and line number information into build files
    In order to support included files, the webview code needs access to
    the source uri in addition to the line number.
    
    Rather than even attempt to force uris to work with CSS classes, the
    `SourceLocationTransform` includes a hidden div with a list of
    `<span>` elements, one for each location marker.
    
    These `<span>` elements have `data-uri` and `data-line` attributes
    containing the necessary information to make sync scrolling work.
    Each element has a `data-id` attribute which corresponds to a CSS
    class `esbonio-marker-<id>` which links the location which its
    corresponding location in the DOM

--- lib/esbonio/esbonio/sphinx_agent/log.py
+++ lib/esbonio/esbonio/sphinx_agent/log.py
@@ -38,73 +38,6 @@ class DiagnosticFilter(logging.Filter):
         self.app = app
         self.diagnostics: Dict[Uri, Set[types.Diagnostic]] = {}
 
-    def get_location(self, location: str) -> Tuple[str, Optional[int]]:
-        if not location:
-            conf = pathlib.Path(self.app.confdir, "conf.py")
-            return (str(conf), None)
-
-        lineno = None
-        path, parts = self.get_location_path(location)
-
-        if len(parts) == 1:
-            try:
-                lineno = int(parts[0])
-            except ValueError:
-                pass
-
-        if len(parts) == 2 and parts[0].startswith("docstring of "):
-            target = parts[0].replace("docstring of ", "")
-            lineno = self.get_docstring_location(target, parts[1])
-
-        return (path, lineno)
-
-    def get_location_path(self, location: str) -> Tuple[str, List[str]]:
-        """Determine the filepath from the given location."""
-
-        if location.startswith("internal padding before "):
-            location = location.replace("internal padding before ", "")
-
-        if location.startswith("internal padding after "):
-            location = location.replace("internal padding after ", "")
-
-        path, *parts = location.split(":")
-
-        # On windows the rest of the path will be the first element of parts
-        if pathlib.Path(location).drive:
-            path += f":{parts.pop(0)}"
-
-        # Diagnostics in .. included:: files are reported relative to the process'
-        # working directory, so ensure the path is absolute.
-        path = os.path.abspath(path)
-
-        return path, parts
-
-    def get_docstring_location(self, target: str, offset: str) -> Optional[int]:
-        # The containing module will be the longest substring we can find in target
-        candidates = [m for m in sys.modules.keys() if target.startswith(m)] + [""]
-        module = sys.modules.get(sorted(candidates, key=len, reverse=True)[0], None)
-
-        if module is None:
-            return None
-
-        obj: Union[ModuleType, Any, None] = module
-        dotted_name = target.replace(module.__name__ + ".", "")
-
-        for name in dotted_name.split("."):
-            obj = getattr(obj, name, None)
-            if obj is None:
-                return None
-
-        try:
-            _, line = inspect.getsourcelines(obj)  # type: ignore
-
-            # Correct off by one error for docstrings that don't start with a newline.
-            nl = (obj.__doc__ or "").startswith("\n")
-            return line + int(offset) - (not nl)
-        except Exception:
-            logger.debug("Unable to determine diagnostic location\n%s", exc_info=True)
-            return None
-
     def filter(self, record: logging.LogRecord) -> bool:
         conditions = [
             "sphinx" not in record.name,
@@ -115,7 +48,12 @@ class DiagnosticFilter(logging.Filter):
             return True
 
         loc = getattr(record, "location", "")
-        doc, lineno = self.get_location(loc)
+        uri, lineno = source_to_uri_and_linum(loc)
+
+        if uri is None:
+            conf = pathlib.Path(self.app.confdir, "conf.py")
+            uri, lineno = (Uri.for_file(conf), None)
+
         line = lineno or 1
 
         try:
@@ -144,5 +82,83 @@ class DiagnosticFilter(logging.Filter):
             ),
         )
 
-        self.diagnostics.setdefault(Uri.for_file(doc), set()).add(diagnostic)
+        self.diagnostics.setdefault(uri, set()).add(diagnostic)
         return True
+
+
+def source_to_uri_and_linum(location: str) -> Tuple[Optional[Uri], Optional[int]]:
+    """Convert the given source location to a uri and corresponding line number
+
+    Parameters
+    ----------
+    location
+       The location to convert
+
+    Returns
+    -------
+    Tuple[Optional[Uri], Optional[int]]
+       The corresponding uri and line number, if known
+    """
+    lineno = None
+    path, parts = _get_location_path(location)
+
+    if len(parts) == 1:
+        try:
+            lineno = int(parts[0])
+        except ValueError:
+            pass
+
+    if len(parts) == 2 and parts[0].startswith("docstring of "):
+        target = parts[0].replace("docstring of ", "")
+        lineno = _get_docstring_linum(target, parts[1])
+
+    return Uri.for_file(path), lineno
+
+
+def _get_location_path(location: str) -> Tuple[str, List[str]]:
+    """Determine the filepath from the given location."""
+
+    if location.startswith("internal padding before "):
+        location = location.replace("internal padding before ", "")
+
+    if location.startswith("internal padding after "):
+        location = location.replace("internal padding after ", "")
+
+    path, *parts = location.split(":")
+
+    # On windows the rest of the path will be the first element of parts
+    if pathlib.Path(location).drive:
+        path += f":{parts.pop(0)}"
+
+    # Diagnostics in .. included:: files are reported relative to the process'
+    # working directory, so ensure the path is absolute.
+    path = os.path.abspath(path)
+
+    return path, parts
+
+
+def _get_docstring_linum(target: str, offset: str) -> Optional[int]:
+    # The containing module will be the longest substring we can find in target
+    candidates = [m for m in sys.modules.keys() if target.startswith(m)] + [""]
+    module = sys.modules.get(sorted(candidates, key=len, reverse=True)[0], None)
+
+    if module is None:
+        return None
+
+    obj: Union[ModuleType, Any, None] = module
+    dotted_name = target.replace(module.__name__ + ".", "")
+
+    for name in dotted_name.split("."):
+        obj = getattr(obj, name, None)
+        if obj is None:
+            return None
+
+    try:
+        _, line = inspect.getsourcelines(obj)  # type: ignore
+
+        # Correct off by one error for docstrings that don't start with a newline.
+        nl = (obj.__doc__ or "").startswith("\n")
+        return line + int(offset) - (not nl)
+    except Exception:
+        logger.debug("Unable to determine diagnostic location\n%s", exc_info=True)
+        return None
