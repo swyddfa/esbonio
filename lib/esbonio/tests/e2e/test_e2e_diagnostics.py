748521d88b97864eae7c9aa8c0c18448eb1ba634
Alex Carney <alcarneyme@gmail.com>
2024-08-06 21:38:40 +0100

    lsp: Remove `furo` as test dependency
    Nothing against furo, but not depending on it for test cases is one
    less dependency to migrate when a major version of Sphinx rolls
    around.
    
    Plus overriding the theme set in `conf.py` is a nice way to exercise
    the `configOverrides` option

--- lib/esbonio/tests/e2e/test_e2e_diagnostics.py
+++ lib/esbonio/tests/e2e/test_e2e_diagnostics.py
@@ -121,6 +121,10 @@ async def pub_client(lsp_client: LanguageClient, uri_for, tmp_path_factory):
                         workspace_uri.fs_path,
                         str(build_dir),
                     ],
+                    "configOverrides": {
+                        "html_theme": "alabaster",
+                        "html_theme_options": {},
+                    },
                     "pythonCommand": [sys.executable],
                 },
             },
