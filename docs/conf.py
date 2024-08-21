07fed3e4a9d310ffa1787a6cc0d9a50c18768c84
Alex Carney <alcarneyme@gmail.com>
2024-08-21 18:23:02 +0000

    docs: Use hatch for building docs

--- docs/conf.py
+++ docs/conf.py
@@ -11,6 +11,7 @@
 import os
 import sys
 
+sys.path.insert(0, os.path.abspath("../lib/esbonio-extensions"))
 sys.path.insert(0, os.path.abspath("../lib/esbonio"))
 sys.path.insert(0, os.path.abspath("./ext"))
 
