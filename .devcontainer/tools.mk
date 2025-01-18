3cad2fef94a2510a0e911fae6f27cdfe7df5784b
Alex Carney <alcarneyme@gmail.com>
2025-01-18 19:49:22 +0000

    devenv: Use uv to bootstrap Python and associated tools

--- .devcontainer/tools.mk
+++ .devcontainer/tools.mk
@@ -6,96 +6,45 @@ $(error Unable to determine platform architecture)
 endif
 
 NODE_VERSION := 20.18.0
+PY_VERSION := 3.13
+UV_VERSION := 0.5.21
 
-# The versions of Python we support
-PYXX_versions := 3.9 3.10 3.11 3.12 3.13
-PY_INTERPRETERS =
+UV ?= $(shell command -v uv)
+UVX ?= $(shell command -v uvx)
+
+ifeq ($(strip $(UV)),)
+
+UV := $(BIN)/uv
+UVX := $(BIN)/uvx
+
+$(UV):
+	curl -L --output /tmp/uv.tar.gz https://github.com/astral-sh/uv/releases/download/$(UV_VERSION)/uv-$(ARCH)-unknown-linux-gnu.tar.gz
+	tar -xf /tmp/uv.tar.gz -C /tmp
+	rm /tmp/uv.tar.gz
+
+	test -d $(BIN) || mkdir -p $(BIN)
+
+	mv /tmp/uv-$(ARCH)-unknown-linux-gnu/uv $@
+	mv /tmp/uv-$(ARCH)-unknown-linux-gnu/uvx $(UVX)
+
+	$@ --version
+	$(UVX) --version
+
+endif
+
 
 # Hatch is not only used for building packages, but bootstrapping any missing
 # interpreters
-HATCH ?= $(or $(shell command -v hatch), $(BIN)/hatch)
+HATCH ?= $(shell command -v hatch)
 
-$(HATCH):
-	curl -L --output /tmp/hatch.tar.gz https://github.com/pypa/hatch/releases/latest/download/hatch-$(ARCH)-unknown-linux-gnu.tar.gz
-	tar -xf /tmp/hatch.tar.gz -C /tmp
-	rm /tmp/hatch.tar.gz
+ifeq ($(strip $(HATCH)),)
 
-	test -d $(BIN) || mkdir -p $(BIN)
-	mv /tmp/hatch $(HATCH)
+HATCH := $(BIN)/hatch
 
+$(HATCH): | $(UV)
+	$(UV) tool install hatch
 	$@ --version
-	touch $@
 
-# This effectively defines a function `PYXX` that takes a Python version number
-# (e.g. 3.8) and expands it out into a common block of code that will ensure a
-# verison of that interpreter is available to be used.
-#
-# The is perhaps a bit more complicated than I'd like, but it should mean that
-# the project's makefiles are useful both inside and outside of a devcontainer.
-#
-# `PYXX` has the following behavior:
-# - If possible, it will reuse the user's existing version of Python
-#   i.e. $(shell command -v pythonX.X)
-#
-# - The user may force a specific interpreter to be used by setting the
-#   variable when running make e.g. PYXX=/path/to/pythonX.X make ...
-#
-# - Otherwise, `make` will use `$(HATCH)` to install the given version of
-#   Python under `$(BIN)`
-#
-# See: https://www.gnu.org/software/make/manual/html_node/Eval-Function.html
-define PYXX =
-
-PY$(subst .,,$1) ?= $$(shell command -v python$1)
-
-ifeq ($$(strip $$(PY$(subst .,,$1))),)
-
-PY$(subst .,,$1) := $$(BIN)/python$1
-
-$$(PY$(subst .,,$1)): $$(HATCH)
-	$$(HATCH) python find $1 || $$(HATCH) python install $1
-	ln -s $$$$($$(HATCH) python find $1) $$@
-
-	$$@ --version
-	touch $$@
-
-endif
-
-PY_INTERPRETERS += $$(PY$(subst .,,$1))
-endef
-
-# Uncomment the following line to see what this expands into.
-#$(foreach version,$(PYXX_versions),$(info $(call PYXX,$(version))))
-$(foreach version,$(PYXX_versions),$(eval $(call PYXX,$(version))))
-
-# Set a default `python` command if there is not one already
-PY ?= $(shell command -v python3)
-
-ifeq ($(strip $(PY)),)
-PY := $(BIN)/python
-
-$(PY): $(PY313)
-	ln -s $< $@
-	$@ --version
-	touch $@
-endif
-
-PY_INTERPRETERS += $(PY)
-#$(info $(PY_INTERPRETERS))
-
-PIPX ?= $(shell command -v pipx)
-
-ifeq ($(strip $(PIPX)),)
-PIPX := $(BIN)/pipx
-PIPX_VERSION := 1.5.0
-
-$(PIPX):
-	curl -L -o $(BIN)/pipx.pyz https://github.com/pypa/pipx/releases/download/$(PIPX_VERSION)/pipx.pyz
-	echo '#!/bin/bash\nexec $(PY) $(BIN)/pipx.pyz "$$@"' > $(PIPX)
-
-	chmod +x $(PIPX)
-	$@ --version
-	touch $@
 endif
 
 PRE_COMMIT ?= $(shell command -v pre-commit)
@@ -103,13 +52,25 @@ PRE_COMMIT ?= $(shell command -v pre-commit)
 ifeq ($(strip $(PRE_COMMIT)),)
 PRE_COMMIT := $(BIN)/pre-commit
 
-$(PRE_COMMIT): $(PIPX)
-	$(PIPX) install pre-commit
+$(PRE_COMMIT): | $(UV)
+	$(UV) tool install pre-commit
 	$@ --version
-	touch $@
+
 endif
 
-PY_TOOLS := $(HATCH) $(PIPX) $(PRE_COMMIT)
+PY_TOOLS := $(HATCH) $(PRE_COMMIT)
+
+# Set a default `python` command if there is not one already
+PY ?= $(shell command -v python)
+
+ifeq ($(strip $(PY)),)
+PY := $(BIN)/python
+
+$(PY): | $(UV)
+	$(UV) python install $(PY_VERSION)
+	ln -s $$($(UV) python find $(PY_VERSION)) $@
+	$@ --version
+endif
 
 # Node JS
 NPM ?= $(shell command -v npm)
