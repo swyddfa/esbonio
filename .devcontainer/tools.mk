9ab95b6711ad0838558b87dd27a0223d90ed2a0f
Alex Carney <alcarneyme@gmail.com>
2025-06-27 19:14:02 +0100

    workflow: Start testing on Python 3.14

--- .devcontainer/tools.mk
+++ .devcontainer/tools.mk
@@ -32,7 +32,7 @@ $(UV):
 endif
 
 # The versions of Python we support
-PYXX_versions := 3.9 3.10 3.11 3.12 3.13
+PYXX_versions := 3.9 3.10 3.11 3.12 3.13 3.14
 
 # Our default Python version
 PY_VERSION := 3.13
