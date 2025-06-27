665bd64c4a9a5f89f7cb287f839cd708e7c1792d
Alex Carney <alcarneyme@gmail.com>
2025-06-27 19:11:49 +0100

    devcontainer: Bump dependencies

--- .devcontainer/tools.mk
+++ .devcontainer/tools.mk
@@ -5,8 +5,8 @@ ifeq ($(strip $(ARCH)),)
 $(error Unable to determine platform architecture)
 endif
 
-NODE_VERSION := 20.18.0
-UV_VERSION := 0.5.21
+NODE_VERSION := 20.19.3
+UV_VERSION := 0.7.15
 
 UV ?= $(shell command -v uv)
 UVX ?= $(shell command -v uvx)
