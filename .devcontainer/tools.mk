31711b3636365f8d443822b9b0ff1723bd9d19aa
Alex Carney <alcarneyme@gmail.com>
2025-09-26 20:40:15 +0100

    devenv: Drop 3.9, bump tool versions

--- .devcontainer/tools.mk
+++ .devcontainer/tools.mk
@@ -5,8 +5,8 @@ ifeq ($(strip $(ARCH)),)
 $(error Unable to determine platform architecture)
 endif
 
-NODE_VERSION := 20.19.4
-UV_VERSION := 0.8.11
+NODE_VERSION := 20.19.5
+UV_VERSION := 0.8.22
 
 UV ?= $(shell command -v uv)
 UVX ?= $(shell command -v uvx)
@@ -32,7 +32,7 @@ $(UV):
 endif
 
 # The versions of Python we support
-PYXX_versions := 3.9 3.10 3.11 3.12 3.13 3.14
+PYXX_versions := 3.10 3.11 3.12 3.13 3.14
 
 # Our default Python version
 PY_VERSION := 3.13
