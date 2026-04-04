3364b980e56a5de3402ca55cc8b1cca924067629
Alex Carney <alcarneyme@gmail.com>
2026-04-05 00:10:08 +0100

    lsp: Set `e.s.completion.preferredInsertBehavior` to `insert` by default

--- lib/esbonio/tests/e2e/conftest.py
+++ lib/esbonio/tests/e2e/conftest.py
@@ -24,7 +24,17 @@ async def client(lsp_client: LanguageClient, uri_for, tmp_path_factory):
 
     # Override the project configuration so that builds are written into a temporary folder
     build_dir = tmp_path_factory.mktemp("build")
-    lsp_client.set_configuration({"logging": {"level": "debug"}}, section="esbonio")
+    lsp_client.set_configuration(
+        {
+            "logging": {"level": "debug"},
+            "server": {
+                "completion": {
+                    "preferredInsertBehavior": "replace",
+                }
+            },
+        },
+        section="esbonio",
+    )
     lsp_client.set_configuration(
         {
             "buildCommand": [
